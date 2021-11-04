eca85c016f8194ed44e336ff52c0685f337d8a15
Skyler Edvik <edvik.skyler@gmail.com>
2021-11-04 15:17:05 -0600

    Remove test code from initial static application from App.js

--- src/App.js
+++ src/App.js
@@ -6,86 +6,13 @@ import './styles/App.css'
 
 class App extends React.Component {
   render () {
-    const contactInfo = {
-      name: 'Karl Dublin',
-      email: 'karl.dublin@yorkshirepudding.com',
-      phone: '555-555-5555'
-    }
-
-    const education = [
-      {
-        schoolName: 'University of Utah',
-        titleOfStudy: 'Chemical Engineering',
-        startDate: '2012-08-21',
-        endDate: '2016-05-10'
-      },
-      {
-        schoolName: 'Granger High',
-        titleOfStudy: 'General',
-        startDate: '2009-08-28',
-        endDate: '2012-05-21'
-      }
-    ]
-
-    const experience = [
-      {
-        companyName: 'Edwards Lifesciences',
-        position: 'Quality Engineer I',
-        tasks: [
-          {
-            id: 1,
-            text: 'Perform IQ/OQ'
-          },
-          {
-            id: 2,
-            text: 'Troubleshoot equipment failures'
-          },
-          {
-            id: 3,
-            text: 'Process Validation'
-          }
-        ],
-        startDate: '2016-05-28',
-        endDate: '2018-04-28'
-      },
-      {
-        companyName: 'Stryker',
-        position: 'Sr. Quality Engineer',
-        tasks: [
-          {
-            id: 1,
-            text: 'Perform IQ/OQ'
-          },
-          {
-            id: 2,
-            text: 'Troubleshoot equipment failures'
-          },
-          {
-            id: 3,
-            text: 'Process Validation'
-          }
-        ],
-        startDate: '2020-03-31',
-        endDate: '2021-04-21'
-      }
-    ]
-
     return (
       <div className="resume-container">
-        <ContactInfo
-          editable={true}
-          contactInfo={contactInfo}
-        />
+        <ContactInfo />
         <hr />
-        <Education
-          editable={true}
-          education={education}
-        />
+        <Education />
         <hr />
-        <Experience
-          editable={true}
-          experience={experience}
-        />
+        <Experience />
       </div>
     )
   }
