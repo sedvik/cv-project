5f63b474acbbbe12ef5590c1f890d7f9c23175a1
Skyler Edvik <edvik.skyler@gmail.com>
2021-11-08 15:14:28 -0700

    Refactor ExperienceDisplayEntry.js to use functional component syntax

--- src/components/ExperienceDisplayEntry.js
+++ src/components/ExperienceDisplayEntry.js
@@ -2,29 +2,27 @@ import React from 'react'
 import TasksDisplay from './TasksDisplay'
 import '../styles/ExperienceDisplayEntry.css'
 
-class ExperienceDisplayEntry extends React.Component {
-  render () {
-    const {
-      companyName,
-      position,
-      tasks,
-      startDate,
-      endDate
-    } = this.props.experienceEntry
+function ExperienceDisplayEntry (props) {
+  const {
+    companyName,
+    position,
+    tasks,
+    startDate,
+    endDate
+  } = props.experienceEntry
 
-    return (
-      <div className="experience-display-entry-container">
-        <div className="company-date-container">
-          <p className="bold">{companyName}</p>
-          <p>
-            <i>{`${startDate}`}</i> <span className="large">-</span> <i>{`${endDate}`}</i>
-          </p>
-        </div>
-        <h2>{position}</h2>
-        <TasksDisplay tasks={tasks} />
+  return (
+    <div className="experience-display-entry-container">
+      <div className="company-date-container">
+        <p className="bold">{companyName}</p>
+        <p>
+          <i>{`${startDate}`}</i> <span className="large">-</span> <i>{`${endDate}`}</i>
+        </p>
       </div>
-    )
-  }
+      <h2>{position}</h2>
+      <TasksDisplay tasks={tasks} />
+    </div>
+  )
 }
 
 export default ExperienceDisplayEntry
