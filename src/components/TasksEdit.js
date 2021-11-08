aa544f8ecb3f6b4391da7eacdb7501ffce889d46
Skyler Edvik <edvik.skyler@gmail.com>
2021-11-08 15:25:25 -0700

    Refactor TasksEdit.js to use functional component syntax

--- src/components/TasksEdit.js
+++ src/components/TasksEdit.js
@@ -2,59 +2,52 @@ import React from 'react'
 import EditableTask from './EditableTask'
 import '../styles/TasksEdit.css'
 
-class TasksEdit extends React.Component {
-  constructor (props) {
-    super(props)
-    this.handleButtonClick = this.handleButtonClick.bind(this)
-  }
+function TasksEdit (props) {
+  const {
+    entryId,
+    tasks,
+    taskCallbacks
+  } = props
 
-  handleButtonClick (e) {
+  const {
+    deleteTaskEntry,
+    updateTaskEntry
+  } = taskCallbacks
+
+  function handleButtonClick (e) {
     const id = e.target.getAttribute('data-entryid')
-    this.props.taskCallbacks.addTaskEntry(id)
+    props.taskCallbacks.addTaskEntry(id)
   }
 
-  render () {
-    const {
-      entryId,
-      tasks,
-      taskCallbacks
-    } = this.props
-
-    const {
-      deleteTaskEntry,
-      updateTaskEntry
-    } = taskCallbacks
-
-    return (
-      <div className="tasks-edit-container">
-        <h2>Tasks</h2>
-        <div className="editable-tasks-container">
-          {
-            tasks.map((task, i) => {
-              return (
-                <EditableTask
-                  key={task.id}
-                  task={task}
-                  entryId={entryId}
-                  taskId={task.id}
-                  entryNum={i + 1}
-                  deleteTaskEntry={deleteTaskEntry}
-                  updateTaskEntry={updateTaskEntry}
-                />
-              )
-            })
-          }
-        </div>
-        <button
-          className="add-task"
-          data-entryid={entryId}
-          onClick={this.handleButtonClick}
-        >
-          Add Task
-        </button>
+  return (
+    <div className="tasks-edit-container">
+      <h2>Tasks</h2>
+      <div className="editable-tasks-container">
+        {
+          tasks.map((task, i) => {
+            return (
+              <EditableTask
+                key={task.id}
+                task={task}
+                entryId={entryId}
+                taskId={task.id}
+                entryNum={i + 1}
+                deleteTaskEntry={deleteTaskEntry}
+                updateTaskEntry={updateTaskEntry}
+              />
+            )
+          })
+        }
       </div>
-    )
-  }
+      <button
+        className="add-task"
+        data-entryid={entryId}
+        onClick={handleButtonClick}
+      >
+        Add Task
+      </button>
+    </div>
+  )
 }
 
 export default TasksEdit
