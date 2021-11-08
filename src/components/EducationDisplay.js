9b2ecd5d9d0b0e8b433d3e3b7b696eeeef96f40c
Skyler Edvik <edvik.skyler@gmail.com>
2021-11-08 14:43:53 -0700

    Refactor EducationDisplay.js to use functional component syntax

--- src/components/EducationDisplay.js
+++ src/components/EducationDisplay.js
@@ -2,30 +2,28 @@ import React from 'react'
 import EducationDisplayEntry from './EducationDisplayEntry'
 import '../styles/EducationDisplay.css'
 
-class EducationDisplay extends React.Component {
-  render () {
-    const { education } = this.props
+function EducationDisplay (props) {
+  const { education } = props
 
-    return (
-      <table className="education-display">
-        <thead>
-          <tr>
-            <th>School</th>
-            <th>Title of Study</th>
-            <th>Start Date</th>
-            <th>End Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            education.map((entry, i) => {
-              return <EducationDisplayEntry key={entry.id} educationEntry={entry} />
-            })
-          }
-        </tbody>
-      </table>
-    )
-  }
+  return (
+    <table className="education-display">
+      <thead>
+        <tr>
+          <th>School</th>
+          <th>Title of Study</th>
+          <th>Start Date</th>
+          <th>End Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          education.map((entry, i) => {
+            return <EducationDisplayEntry key={entry.id} educationEntry={entry} />
+          })
+        }
+      </tbody>
+    </table>
+  )
 }
 
 export default EducationDisplay
