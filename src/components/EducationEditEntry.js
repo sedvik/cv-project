269a23c07b20d1e92e4bca565d9abb0d9cc2c757
Skyler Edvik <edvik.skyler@gmail.com>
2021-11-08 14:51:51 -0700

    Refactor EducationEditEntry.js to use functional component syntax

--- src/components/EducationEditEntry.js
+++ src/components/EducationEditEntry.js
@@ -1,76 +1,68 @@
 import React from 'react'
 import '../styles/EducationEditEntry.css'
 
-class EducationEditEntry extends React.Component {
-  constructor (props) {
-    super(props)
-    this.handleEntryUpdate = this.handleEntryUpdate.bind(this)
-    this.handleEntryDelete = this.handleEntryDelete.bind(this)
-  }
+function EducationEditEntry (props) {
+  const {
+    id,
+    schoolName,
+    titleOfStudy,
+    startDate,
+    endDate
+  } = props.educationEntry
 
-  handleEntryUpdate (e) {
+  function handleEntryUpdate (e) {
     const id = e.target.getAttribute('data-id')
     const property = e.target.getAttribute('data-property')
     const value = e.target.value
-    this.props.updateEducationEntry(id, property, value)
+    props.updateEducationEntry(id, property, value)
   }
 
-  handleEntryDelete (e) {
+  function handleEntryDelete (e) {
     const id = e.target.getAttribute('data-id')
-    this.props.deleteEducationEntry(id)
+    props.deleteEducationEntry(id)
   }
 
-  render () {
-    const {
-      id,
-      schoolName,
-      titleOfStudy,
-      startDate,
-      endDate
-    } = this.props.educationEntry
-
-    return (
-      <div className="education-card">
-          <button data-id={id} onClick={this.handleEntryDelete}>X</button>
-          <label htmlFor={`school-${id}`}>School</label>
-          <input
-            id={`school-${id}`}
-            type="text"
-            defaultValue={schoolName}
-            data-id={id}
-            data-property="schoolName"
-            onChange={this.handleEntryUpdate}
-          />
-          <label htmlFor={`title-${id}`}>Title of Study</label>
-          <input
-            id={`title-${id}`}
-            type="text"
-            defaultValue={titleOfStudy}
-            data-id={id}
-            data-property="titleOfStudy"
-            onChange={this.handleEntryUpdate}
-          />
-          <label htmlFor={`edu-start-date-${id}`}>Start Date</label>
-          <input
-            id={`edu-start-date-${id}`}
-            type="date"
-            defaultValue={startDate}
-            data-id={id}
-            data-property="startDate"
-            onChange={this.handleEntryUpdate}
-          />
-          <label htmlFor={`edu-end-date-${id}`}>End Date</label>
-          <input
-            id={`edu-end-date-${id}`}
-            type="date"
-            defaultValue={endDate}
-            data-id={id}
-            data-property="endDate"
-            onChange={this.handleEntryUpdate}
-          />
-      </div>
-    )
-  }
+  return (
+    <div className="education-card">
+        <button data-id={id} onClick={handleEntryDelete}>X</button>
+        <label htmlFor={`school-${id}`}>School</label>
+        <input
+          id={`school-${id}`}
+          type="text"
+          defaultValue={schoolName}
+          data-id={id}
+          data-property="schoolName"
+          onChange={handleEntryUpdate}
+        />
+        <label htmlFor={`title-${id}`}>Title of Study</label>
+        <input
+          id={`title-${id}`}
+          type="text"
+          defaultValue={titleOfStudy}
+          data-id={id}
+          data-property="titleOfStudy"
+          onChange={handleEntryUpdate}
+        />
+        <label htmlFor={`edu-start-date-${id}`}>Start Date</label>
+        <input
+          id={`edu-start-date-${id}`}
+          type="date"
+          defaultValue={startDate}
+          data-id={id}
+          data-property="startDate"
+          onChange={handleEntryUpdate}
+        />
+        <label htmlFor={`edu-end-date-${id}`}>End Date</label>
+        <input
+          id={`edu-end-date-${id}`}
+          type="date"
+          defaultValue={endDate}
+          data-id={id}
+          data-property="endDate"
+          onChange={handleEntryUpdate}
+        />
+    </div>
+  )
 }
 
 export default EducationEditEntry
