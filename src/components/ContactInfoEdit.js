db19efc1f98096baeca04e4cfc2a1ba587543084
Skyler Edvik <edvik.skyler@gmail.com>
2021-11-08 14:23:43 -0700

    Refactor ContactInfoEdit.js to use functional component syntax

--- src/components/ContactInfoEdit.js
+++ src/components/ContactInfoEdit.js
@@ -1,58 +1,49 @@
 import React from 'react'
 import '../styles/ContactInfoEdit.css'
 
-class ContactInfoEdit extends React.Component {
-  constructor (props) {
-    super(props)
-    this.handleNameChange = this.handleNameChange.bind(this)
-    this.handleEmailChange = this.handleEmailChange.bind(this)
-    this.handlePhoneChange = this.handlePhoneChange.bind(this)
-  }
+function ContactInfoEdit (props) {
+  const { name, email, phone } = props.contactInfo
 
-  handleNameChange (e) {
+  function handleNameChange (e) {
     const newValue = e.target.value
-    this.props.updateContactInfo('name', newValue)
+    props.updateContactInfo('name', newValue)
   }
 
-  handleEmailChange (e) {
+  function handleEmailChange (e) {
     const newValue = e.target.value
-    this.props.updateContactInfo('email', newValue)
+    props.updateContactInfo('email', newValue)
   }
 
-  handlePhoneChange (e) {
+  function handlePhoneChange (e) {
     const newValue = e.target.value
-    this.props.updateContactInfo('phone', newValue)
+    props.updateContactInfo('phone', newValue)
   }
 
-  render () {
-    const { name, email, phone } = this.props.contactInfo
-
-    return (
-      <div className='contact-info-edit'>
-        <label htmlFor="contact-name">Name</label>
-        <input
-          id="contact-name"
-          type="text"
-          defaultValue={name}
-          onChange={this.handleNameChange}
-        />
-        <label htmlFor="contact-email">Email</label>
-        <input
-          id="contact-email"
-          type="email"
-          defaultValue={email}
-          onChange={this.handleEmailChange}
-        />
-        <label htmlFor="contact-phone">Phone Number</label>
-        <input
-          id="contact-phone"
-          type="tel"
-          defaultValue={phone}
-          onChange={this.handlePhoneChange}
-        />
-      </div>
-    )
-  }
+  return (
+    <div className='contact-info-edit'>
+      <label htmlFor="contact-name">Name</label>
+      <input
+        id="contact-name"
+        type="text"
+        defaultValue={name}
+        onChange={handleNameChange}
+      />
+      <label htmlFor="contact-email">Email</label>
+      <input
+        id="contact-email"
+        type="email"
+        defaultValue={email}
+        onChange={handleEmailChange}
+      />
+      <label htmlFor="contact-phone">Phone Number</label>
+      <input
+        id="contact-phone"
+        type="tel"
+        defaultValue={phone}
+        onChange={handlePhoneChange}
+      />
+    </div>
+  )
 }
 
 export default ContactInfoEdit
