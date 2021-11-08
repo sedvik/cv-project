f4706f58d2392dfcf7c3209ae1e7806f61f4f89e
Skyler Edvik <edvik.skyler@gmail.com>
2021-11-08 14:27:49 -0700

    Refactor SubmitBtn.js to use functional component syntax

--- src/components/SubmitBtn.js
+++ src/components/SubmitBtn.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-class SubmitBtn extends React.Component {
-  render () {
-    const { setToDisplay } = this.props
+function SubmitBtn (props) {
+  const { setToDisplay } = props
 
-    return <button onClick={setToDisplay}>Submit</button>
-  }
+  return <button onClick={setToDisplay}>Submit</button>
 }
 
 export default SubmitBtn
