2eeb0a3a9ed35f324b88e89c8bf6d3aad60d683f
Skyler Edvik <edvik.skyler@gmail.com>
2021-11-08 15:15:58 -0700

    Refactor TasksDisplay.js to use functional component syntax

--- src/components/TasksDisplay.js
+++ src/components/TasksDisplay.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import '../styles/TasksDisplay.css'
 
-class TasksDisplay extends React.Component {
-  render () {
-    const { tasks } = this.props
+function TasksDisplay (props) {
+  const { tasks } = props
 
-    return (
-      <div className="tasks-display">
-        <ul>
-          {tasks.map((task, i) => {
-            return <li key={i}>{task.text}</li>
-          })}
-        </ul>
-      </div>
-    )
-  }
+  return (
+    <div className="tasks-display">
+      <ul>
+        {tasks.map((task, i) => {
+          return <li key={i}>{task.text}</li>
+        })}
+      </ul>
+    </div>
+  )
 }
 
 export default TasksDisplay
