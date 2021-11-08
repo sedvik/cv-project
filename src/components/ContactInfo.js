4fc94f827db9ffe40aabded86a906b3fe01c263c
Skyler Edvik <edvik.skyler@gmail.com>
2021-11-08 14:11:33 -0700

    Refactor ContactInfo.js to use functional component syntax and state hook

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,64 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ContactInfoDisplay from './ContactInfoDisplay'
 import ContactInfoEdit from './ContactInfoEdit'
 import EditBtn from './EditBtn'
 import SubmitBtn from './SubmitBtn'
 import '../styles/ContactInfo.css'
 
-class ContactInfo extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      editable: true,
-      contactInfo: {
-        name: '',
-        email: '',
-        phone: ''
+function ContactInfo () {
+  const [editable, setEditable] = useState(true)
+  const [contactInfo, setContactInfo] = useState({
+    name: '',
+    email: '',
+    phone: ''
+  })
+
+  function setToEditable () {
+    setEditable(true)
+  }
+
+  function setToDisplay () {
+    setEditable(false)
+  }
+
+  function updateContactInfo (property, value) {
+    const updatedContactInfo = Object.assign(
+      {},
+      contactInfo,
+      {
+        [property]: value
       }
-    }
-    this.setEditable = this.setEditable.bind(this)
-    this.setToDisplay = this.setToDisplay.bind(this)
-    this.updateContactInfo = this.updateContactInfo.bind(this)
-  }
-
-  setEditable () {
-    this.setState({
-      editable: true
-    })
-  }
-
-  setToDisplay () {
-    this.setState({
-      editable: false
-    })
-  }
-
-  updateContactInfo (property, value) {
-    this.setState((prevState, props) => {
-      const updatedContactInfo = Object.assign({}, prevState.contactInfo, { [property]: value })
-      return {
-        contactInfo: updatedContactInfo
-      }
-    })
-  }
-
-  render () {
-    const { editable, contactInfo } = this.state
-
-    return (
-      <div className="contact-info-container">
-        <h1>Contact Information</h1>
-        {editable
-          ? <ContactInfoEdit contactInfo={contactInfo} updateContactInfo={this.updateContactInfo} />
-          : <ContactInfoDisplay contactInfo={contactInfo} />
-        }
-        {editable
-          ? <SubmitBtn setToDisplay={this.setToDisplay} />
-          : <EditBtn setEditable={this.setEditable} />
-        }
-      </div>
     )
+    setContactInfo(updatedContactInfo)
   }
+  return (
+    <div className="contact-info-container">
+      <h1>Contact Information</h1>
+      {editable
+        ? <ContactInfoEdit contactInfo={contactInfo} updateContactInfo={updateContactInfo} />
+        : <ContactInfoDisplay contactInfo={contactInfo} />
+      }
+      {editable
+        ? <SubmitBtn setToDisplay={setToDisplay} />
+        : <EditBtn setEditable={setToEditable} />
+      }
+    </div>
+  )
 }
 
 export default ContactInfo
