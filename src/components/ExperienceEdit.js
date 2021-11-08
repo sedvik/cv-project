2dcba9be53617ad4a923550169ba235d8231e8e4
Skyler Edvik <edvik.skyler@gmail.com>
2021-11-08 15:17:45 -0700

    Refactor ExperienceEdit.js to use functional component syntax

--- src/components/ExperienceEdit.js
+++ src/components/ExperienceEdit.js
@@ -3,35 +3,33 @@ import ExperienceEditEntry from './ExperienceEditEntry'
 import AddEntryBtn from './AddEntryBtn'
 import '../styles/ExperienceEdit.css'
 
-class ExperienceEdit extends React.Component {
-  render () {
-    const {
-      experience,
-      addExperienceEntry,
-      deleteExperienceEntry,
-      updateExperienceEntry,
-      taskCallbacks
-    } = this.props
+function ExperienceEdit (props) {
+  const {
+    experience,
+    addExperienceEntry,
+    deleteExperienceEntry,
+    updateExperienceEntry,
+    taskCallbacks
+  } = props
 
-    return (
-      <div className="experience-edit-container">
-        {
-          experience.map((entry) => {
-            return (
-              <ExperienceEditEntry
-                key={entry.id}
-                experienceEntry={entry}
-                deleteExperienceEntry={deleteExperienceEntry}
-                updateExperienceEntry={updateExperienceEntry}
-                taskCallbacks={taskCallbacks}
-              />
-            )
-          })
-        }
-        <AddEntryBtn addEntry={addExperienceEntry} />
-      </div>
-    )
-  }
+  return (
+    <div className="experience-edit-container">
+      {
+        experience.map((entry) => {
+          return (
+            <ExperienceEditEntry
+              key={entry.id}
+              experienceEntry={entry}
+              deleteExperienceEntry={deleteExperienceEntry}
+              updateExperienceEntry={updateExperienceEntry}
+              taskCallbacks={taskCallbacks}
+            />
+          )
+        })
+      }
+      <AddEntryBtn addEntry={addExperienceEntry} />
+    </div>
+  )
 }
 
 export default ExperienceEdit
